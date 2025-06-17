a137156e2c3f3683e876a5f2787ca80bb50d1e82
Fazlul <fkcfazlul.fk@gmail.com>
2025-06-17 18:45:14 +0530

    paper sight image fixed

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -142,7 +142,7 @@ export const DATA = {
           icon: <Icons.github className="size-3" />,
         },
       ],
-      image: "/papersight.png",
+      image: "/paperSight.png",
       video: "",
     },
     {
